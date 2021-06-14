de33cca976a79756ab49144b90854fe061aa0ec9
tnfy10 <tnfy10@gmail.com>
2021-06-14 18:34:15 +0900

    다운로드 경로 변경

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,35 +1,17 @@
 ﻿using hospi_web_project.Models;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace hospi_web_project.Controllers
 {
     public class HomeController : Controller
     {
-        /*
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
-        */
-
-        private IWebHostEnvironment Environment;
-
-        public HomeController(IWebHostEnvironment _environment)
-        {
-            Environment = _environment;
         }
 
         public IActionResult Index()
@@ -55,9 +37,7 @@ namespace hospi_web_project.Controllers
 
         public ActionResult DownloadFile(string filePath)
         {
-            string wwwPath = this.Environment.WebRootPath;
-            // 다운로드 되어야 할, 서버에 존재하는 파일 경로 
-            string fullName = wwwPath + "\\Download\\" + filePath;
+            string fullName = @"D:\hospi\files\" + filePath;
 
             byte[] fileBytes = GetFile(fullName);
             return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
