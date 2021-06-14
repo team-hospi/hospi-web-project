21e7990b3bb95a4caeafeaabe147308ff0c36fb8
홍성휘 <tnfy10@gmail.com>
2021-06-14 17:19:54 +0900

    다운로드 추가

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,43 @@
 ﻿using hospi_web_project.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace hospi_web_project.Controllers
 {
     public class HomeController : Controller
     {
+        /*
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+        */
+
+        private IWebHostEnvironment Environment;
+
+        public HomeController(IWebHostEnvironment _environment)
+        {
+            Environment = _environment;
+        }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Download()
         {
             return View();
         }
@@ -32,6 +51,26 @@ namespace hospi_web_project.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult DownloadFile(string filePath)
+        {
+            string wwwPath = this.Environment.WebRootPath;
+            // 다운로드 되어야 할, 서버에 존재하는 파일 경로 
+            string fullName = wwwPath + "\\Download\\" + filePath;
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
         }
     }
 }
