f00fac3676e66b6d7e0085923b61644119553c2c
tnfy10 <tnfy10@gmail.com>
2021-09-01 22:18:13 +0900

    마이페이지 구현중

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hospi_web_project.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+
+        public IActionResult UserInfo()
+        {
+            return View();
+        }
+
+        public IActionResult HospitalManagement()
+        {
+            return View();
+        }
+    }
+}
