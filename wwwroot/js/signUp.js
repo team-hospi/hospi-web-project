71545c1d88abde8005f87104ff8991d53518c156
tnfy10 <tnfy10@gmail.com>
2021-09-09 22:10:03 +0900

    회원가입 관련 js파일 추가

---
+++ wwwroot/js/signUp.js
@@ -0,0 +1,24 @@
+﻿function signUpBtn() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var sex_length = document.getElementsByName("sex").length;
+    var sex;
+    for (var i = 0; i < sex_length; i++) {
+        if (document.getElementsByName("sex")[i].checked == true) {
+            sex = document.getElementsByName("sex")[i].value;
+        }
+    }
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var birth = year + "-" + month + "-" + day;
+    var phone = document.getElementById("phone").value;
+
+    document.getElementById("_email").value = email;
+    document.getElementById("_password").value = password;
+    document.getElementById("_sex").value = sex;
+    document.getElementById("_birth").value = birth;
+    document.getElementById("_phone").value = phone;
+
+    return true;
+}
