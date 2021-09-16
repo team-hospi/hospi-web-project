05cf9347e4d152ca6662e3e71568eaf342b780f5
tnfy10 <tnfy10@gmail.com>
2021-09-17 02:35:36 +0900

    다운로드 마이페이지로 이동, 결제 성공 페이지 Hospi 다운 버튼 링크 수정, 마이페이지 수정, 메인 페이지 다운로드 버튼 문의게시판으로 변경

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -21,11 +21,6 @@ namespace hospi_web_project.Controllers
             return View();
         }
 
-        public IActionResult Download()
-        {
-            return View();
-        }
-
         public IActionResult Purchase()
         {
             DBService dbService = HttpContext.RequestServices.GetService(typeof(DBService)) as DBService;
@@ -53,24 +48,6 @@ namespace hospi_web_project.Controllers
             context.PaymentProcess(model);
 
             return RedirectToAction("PaymentSuccess", "Product");
-        }
-
-        public ActionResult DownloadFile(string filePath)
-        {
-            string fullName = @"C:\hospi\files\" + filePath;
-
-            byte[] fileBytes = GetFile(fullName);
-            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
-        }
-
-        byte[] GetFile(string s)
-        {
-            System.IO.FileStream fs = System.IO.File.OpenRead(s);
-            byte[] data = new byte[fs.Length];
-            int br = fs.Read(data, 0, data.Length);
-            if (br != fs.Length)
-                throw new System.IO.IOException(s);
-            return data;
         }
     }
 }
