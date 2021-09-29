8b882e2fd4dcd1d3b94559efdcffc612c00ce79f
tnfy10 <tnfy10@gmail.com>
2021-09-29 15:45:03 +0900

    필요없는 코드 삭제

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -71,24 +71,5 @@ namespace hospi_web_project.Controllers
         {
             return View();
         }
-
-        [Authorize]
-        public ActionResult DownloadFile(string filePath)
-        {
-            string fullName = @"C:\hospi\files\" + filePath;
-
-            byte[] fileBytes = GetFile(fullName);
-            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
-        }
-
-        byte[] GetFile(string s)
-        {
-            System.IO.FileStream fs = System.IO.File.OpenRead(s);
-            byte[] data = new byte[fs.Length];
-            int br = fs.Read(data, 0, data.Length);
-            if (br != fs.Length)
-                throw new System.IO.IOException(s);
-            return data;
-        }
     }
 }
